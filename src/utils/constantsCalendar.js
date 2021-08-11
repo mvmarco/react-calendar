65827c0294e0603ea3c5cb858990013effbf34a0
Marco Corapi <marco.corapi@outlook.it>
2021-08-11 12:43:28 +0200

    started calendar created components and utils functions

---
+++ src/utils/constantsCalendar.js
@@ -0,0 +1,7 @@
+export const DATE = new Date();
+export const WEEK = ["M","T", "O","T","F","L","S"];
+export const MONTH = ["Januar","Februar","Marts","April","Maj","Juni","Juli","August","September", "Oktober", "November", "December"];
+
+export const WEEKSINYEAR = 54;
+export const MONTHSINYEAR = 12;
+export const DAYSINWEEK = 7;
